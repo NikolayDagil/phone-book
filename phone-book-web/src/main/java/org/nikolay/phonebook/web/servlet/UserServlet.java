3a0c2d91a488a661944bcd6acff3e5fb241502b1
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2013-02-22 13:55:51 +0300

    create others Servlets

--- phone-book-web/src/main/java/org/nikolay/phonebook/web/servlet/UserServlet.java
+++ phone-book-web/src/main/java/org/nikolay/phonebook/web/servlet/UserServlet.java
@@ -1,12 +1,16 @@
 package org.nikolay.phonebook.web.servlet;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.nikolay.phonebook.web.constant.PageConstant;
 
 /**
  * @author Nikolay Dagil
@@ -22,6 +26,17 @@ public class UserServlet extends HttpServlet {
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+    Enumeration parameterNames = request.getParameterNames();
+
+    String resultPage = null;
+    if (parameterNames.hasMoreElements() && !request.getParameter("userId").isEmpty()) {
+      request.setAttribute("userId", request.getParameter("userId"));
+      resultPage = PageConstant.USER;
+    } else {
+      resultPage = PageConstant.USER_REGISTRATION;
+    }
+
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher(resultPage);
+    requestDispatcher.forward(request, response);
   }
 }
--- phone-book-web/src/main/java/org/nikolay/phonebook/web/servlet/UsersServlet.java
+++ phone-book-web/src/main/java/org/nikolay/phonebook/web/servlet/UsersServlet.java
@@ -1,11 +1,20 @@
 package org.nikolay.phonebook.web.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.nikolay.phonebook.api.dao.IUserDAO;
+import org.nikolay.phonebook.api.dao.factory.IDAOFactory;
+import org.nikolay.phonebook.api.entity.User;
+import org.nikolay.phonebook.dpl.dao.factory.DAOFactory;
+import org.nikolay.phonebook.web.constant.PageConstant;
 
 /**
  * @author Nikolay Dagil
@@ -21,6 +30,15 @@ public class UsersServlet extends HttpServlet {
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+    IDAOFactory daoFactory = new DAOFactory();
+    Object daoObjectForUser = daoFactory.getDAO("UserDAO");
+
+    IUserDAO userDAO = (IUserDAO) daoObjectForUser;
+
+    List<User> users = userDAO.getAll();
+
+    request.setAttribute("users", users);
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher(PageConstant.USERS);
+    requestDispatcher.forward(request, response);
   }
 }
